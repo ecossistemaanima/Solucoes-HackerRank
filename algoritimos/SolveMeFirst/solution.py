59ecd3646d5c03ee3a8a84d3688a506c1c8c8694
Guilherme Sampaio <guilhermefs09@protonmail.com>
2024-07-10 16:42:45 -0300

    config: __name__==__main__ added

--- algoritimos/SolveMeFirst/solution.py
+++ algoritimos/SolveMeFirst/solution.py
@@ -1,8 +1,8 @@
 def solveMeFirst(a,b):
     return a + b
 
-
-num1 = int(input())
-num2 = int(input())
-res = solveMeFirst(num1,num2)
-print(res)
+if __name__ == '__main__':
+    num1 = int(input())
+    num2 = int(input())
+    res = solveMeFirst(num1,num2)
+    print(res)
